5d06c291906fd7af6550a26b96c33b779f538166
Nathan (Nate) Day <nathancday@gmail.com>
2020-04-04 08:31:00 -0400

    Vrbo (#9)
    * vrbo is crawling
    
    * single space pivot
    
    * working single listing scraper
    
    * sad checkpoint: scraping a lat/lon that isn't specific enough
    
    * happy checkpoint: 121 locations scraped
    
    * report
    
    * explorer report
    
    * listing features viz
    
    * mon-fri date filter added
    
    * more explore and better readme for diff scrapes
    
    * typo fix to knit
    
    * new data for first week in October
    
    * new data for first week of December
    
    * more weekly scrapes plus reports
    
    * Twisted security bump

--- scrapy-splash/README.md
+++ scrapy-splash/README.md
@@ -1,3 +1,49 @@
 # Scrapy-Splash
 
-The original work is [https://github.com/adodd202/Airbnb_Scraping]().
+The original scraper work is from this repo bu [Alex Dodd](https://github.com/adodd202/Airbnb_Scraping). This project would not have been possible without their contribution.
+
+To run either the `airbnb` or `vrbo` scraper:
+
+```
+# fire ups the spash backend
+docker run -p 8050:850 scrapinghub/splash
+
+# change into the proper project
+cd airbnb
+
+# release the spider!
+scrapy crawl airbnb_spider
+```
+
+This will generate the output file `airbnb_cville.csv`.
+
+### Scrapes
+
+| path | desc | date (ish) |
+| --- | --- | --- |
+| airbnb | bounding box restricted to Charlottesville | 2019-01-01 |
+| vrbo | bounding box restricted to Charlottesville | 2019-02-01 |
+| airbnb-oct | airbnb + date filetered Mon(2020-10-05) - Friday(2020-10-09) | 2019-02-24 |
+| airbnb-nov | airbnb + date filetered Mon(2020-11-02) - Friday(2020-11-06) | 2019-02-18 |
+| airbnb-dec | airbnb + date filetered Mon(2020-12-07) - Friday(2020-12-11) | 2019-02-23 |
+
+### Scrapy shell
+
+If you need to interactively explore a page use the [shell](https://docs.scrapy.org/en/latest/topics/shell.html). This is very useful if you want to interactively test code to extract certain page elements.
+
+``` py
+url = 'https://www.vrbo.com/search/keywords:charlottesville-va-usa/@37.999184739322565,-78.53343213198241,38.07003327487784,-78.43695843813475,13z?petIncluded=false&ssr=true&adultsCount=2'
+
+scrapy shell url
+```
+That `url` is for a single of the listings in Charlottesville VA with the default search params (petIncluded, adultsCount) and a map area restriction by lattitude and longitude.
+
+If you want to try and extract chunks of the page you could do something like this in the interactive shell that is opened.
+
+```py
+response.xpath("//span[contains(@class, 'listing-bullets')]/text()").getall()
+```
+
+### Scraped data
+
+The code lives in `explore.R`. It brings together the individual scrape results and does some exploratory plots. It generates a webpage report, `explore.html`, and a santitized table combining both sources, `scraped_rentals.csv`.
---
+++ scrapy-splash/vrbo-nov/vrbo/spiders/vrbo_spider.py
@@ -0,0 +1,88 @@
+from scrapy import Spider, Request
+from vrbo.items import VrboItem
+from scrapy_splash import SplashRequest
+#from selenium import webdriver
+import re
+import csv
+
+
+#################################  IMPORTANT  ################################################
+# When running this script, need to run this command in the background on a separate terminal:
+# docker run -p 8050:8050 scrapinghub/splash
+#################################  IMPORTANT  ################################################
+
+class VrboSpider(Spider):
+
+	name = "vrbo_spider"
+	allowed_urls = ["https://vrbo.com"]
+	start_urls = ["https://vrbo.com"]
+
+	def parse(self, response):
+
+		room_ids = []
+
+		with open("manual_room_scrape.csv", newline="") as file:
+			read = csv.reader(file)
+			for row in read:
+				room_ids.append(row[0])
+
+
+	# This will become a full array of IDs and a for loop
+		for room_id in room_ids:
+			#room_id = "7388327ha" 
+			# url = "https://www.vrbo.com/7160478ha?noDates=true"
+			base_url = "https://www.vrbo.com/"
+			suffix_filters = "?noDates=true"
+
+			url = base_url + room_id + suffix_filters
+
+			yield SplashRequest(url, callback=self.parse_room, args = {"wait": 5}, endpoint = "render.html") 
+			
+	def parse_room(self, response):	
+		item = VrboItem()
+
+		#Extracting the roomID from url.
+		try:
+			roomID = re.search('com/(.*)\?.*', str(response.url)).group(1)
+		except AttributeError:
+			roomID = ''
+
+		try:
+			# rating = re.search('Rated ([0-5](.[0-9])?) out of 5', string1).group(1)
+			details = response.xpath("//span[contains(@class, 'listing-bullets')]/text()").getall()
+		except AttributeError:
+			details = ''
+
+		try:
+			rating = response.xpath("//div/span[contains(@class, 'reviews-summary')]/text()").get()
+		except AttributeError:
+			rating = ''
+
+		try:
+			numReviews = response.xpath("//a/strong[contains(@class, 'num-reviews')]/text()").get()
+		except AttributeError:
+			numReviews = ''
+		
+		try:
+			price = response.xpath('//meta[@property = "og:price:amount"]/@content').get()
+		except AttributeError:
+			price = ''
+
+		try:
+			# I think this is JSON, but it's web messy. Lots of useful info in here like availablity by day for future
+			x = response.xpath('//script[contains(., "router")]/text()').get()
+			lat = re.findall(r'"geoCode":{.*,"latitude":(-?\d+.\d+),', x)
+			lon = re.findall(r'"geoCode":{.*,"longitude":(-\d+.\d+)}', x)
+		except:
+			lat = "?"
+			lon = "?"
+
+		item['roomID'] = roomID
+		item['details'] = details
+		item['rating'] = rating
+		item['numReviews'] = numReviews
+		item['price'] = price
+		item['latitude'] = lat
+		item['longitude'] = lon
+
+		yield item
